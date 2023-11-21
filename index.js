782636492c102293ff8cec5c758bda760bb39ce4
balijawa hussein <balijawahussein@gmail.com>
2023-11-21 14:32:39 +0300

    made the comment system

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const app = express();
 app.set('view engine', 'ejs'); // Corrected line
 app.use(bodyParser.urlencoded({extended:true}));
  
+const personData =[];
 
 app.get("/", function(req, res){
-const name = "";
-const occupation = "";
-const comment = "";
-    res.render("index", { name: name , occupation : occupation, comment :comment});
+    if (personData.length > 0) {
+        res.render("index", { personData: personData });
+    } else {
+        res.render("index", { personData: [] }); // Pass an empty array if personData is empty
+    }
 });
+
  app.post("/",(req, res)=>{
-    const name = req.body.names;
-const occupation = req.body.occupations;
-const comment = req.body.comments;
-    res.render("index", { name: name , occupation : occupation, comment :comment});
+   console.log(req.body);
+   var newPersonData = req.body;
+   personData.push(newPersonData);
+   console.log(personData);
+    res.redirect("/");
  });
+
 app.listen(5000, function(){
     console.log("we are live baby");
 });
