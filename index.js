b8b0664b616b2dcf71d976b665782010cfd45514
balijawa hussein <balijawahussein@gmail.com>
2023-11-21 17:10:33 +0300

    made the boilerplate

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-app.get("/",(req, res)=>{
-    res.send("we are alive");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs'); // Corrected line
+
+app.get("/", function (req, res) {
+    res.render("index", { name: "hussein" });
 });
+
 const port = 5000;
 app.listen(port, console.log("we are running on port 5000"));
